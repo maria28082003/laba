c173e67b6a37cdc607a3c6ae3819dd721f898c33
maria28082003 <72791150+maria28082003@users.noreply.github.com>
2020-12-06 20:31:45 +0200

    Add files via upload

---
+++ ConsoleApplication51/ConsoleApplication51/ConsoleApplication51.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f (const double a, const double b, const double c); // прототип
+
+int main()
+{
+	double s, t;
+
+		cout << "s = "; cin >> s;
+	    cout << "t = "; cin >> t;
+
+	double q = (f(1, t+s ,s) + f(t, s*t ,1))/(1 +  f (s,1,t)*f(s,1,t));
+
+
+	cout << "q = " << q << endl;
+
+	return 0;
+}
+double f(const double a, const double b, const double c) // визначення
+{
+	return (a*sin(b)+ b*sin(a)+ c*c);
+}
